a2f653ac3e83ba73e8676feaddb0a341b2e41322
Canop <cano.petrole@gmail.com>
2014-01-23 15:19:35 +0100

    Loud ping. New design of home page.

--- static/miaou.notify.js
+++ static/miaou.notify.js
@@ -1,30 +1,46 @@
 // manages desktop notifications
 var miaou = miaou || {};
 (function(){
-	miaou.wantNotifs = localStorage['wantNotifs']==="true" || false; // TODO set on a per room basis
+	var audio = new Audio('static/119472-ping.wav'),
+		levels = ["none", "silent", "loud"],
+		level = localStorage['notification'];
+	if (!~levels.indexOf(level)) {
+		level = localStorage['notification'] = "none";
+		// compatibility with ancient boolean storage, this will disapear
+		if (localStorage['wantNotifs']==="true") {
+			level = localStorage['notification'] = "silent";
+		}
+	}
+	console.log('notif level:', level);
 	
 	$(function(){
-		$pref = $('<span class=pref>');
-		$pref.append(
-			$('<input id=pref_notif type=checkbox>').prop('checked', miaou.wantNotifs).change(function(){
-				localStorage['wantNotifs'] = miaou.wantNotifs = this.checked;
-				if (miaou.wantNotifs && Notification.permission !== "granted") {
+		var $switch = $('<div id=ping>').addClass('radios').appendTo('#prefs');
+		$('<span>').addClass('label').text('ping :').appendTo($switch);
+		levels.forEach(function(l){
+			var $r = $('<span>').addClass('radio').text(l).click(function(){
+				$(this).addClass('selected').siblings('.radio').removeClass('selected');
+				level = localStorage['notification'] = l;
+				if (level !== "none" && Notification.permission !== "granted") {
 					Notification.requestPermission(function (permission) {
 						if (!('permission' in Notification)) { // from the MDN - not sure if useful
 							Notification.permission = permission; 
 						}
 					});
 				}
-			})
-		).append(
-			$('<label for=pref_notif>').text("Be notified on ping")
-		).appendTo('#prefs');
+				console.log('notif level:', level);
+			}).appendTo($switch);
+			if (l===level) $r.addClass('selected');
+		});
 	});
 	
 	miaou.notify = function(room, authorname, content){
-		if (!miaou.wantNotifs) return;
+		if (level==="none") return;
 		var n = new Notification(authorname + ' in ' + room.name, {body: content});
 		setTimeout(function(){ n.close() }, 15000);
 		n.onclick = function() { window.focus(); n.close(); };
+		if (level==="loud") {
+			console.log('loud ping!');
+			audio.play();
+		}
 	}
 })();
