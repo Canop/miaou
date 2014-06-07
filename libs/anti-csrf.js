eb285347e1b4639e402a3c38181d240bb9b76651
Canop <cano.petrole@gmail.com>
2014-06-07 18:45:21 +0200

    we now check the referer to prevent CSRF attacks

--- libs/anti-csrf.js
+++ libs/anti-csrf.js
@@ -3,17 +3,20 @@
 // - checking a secret session token in POST requests
 // This implies that all modules must
 // - do all actions on POST forms
-// - insert in the forms a hidden parameter
-// Example in a form in Jade :
-//  input(type="hidden", name="secret", value=secret)
+// - send the POST request for a page with same URL
+// - insert in the forms a hidden parameter :
+//    input(type="hidden", name="secret", value=secret)
 
 module.exports = function(req, res, next){
 	var session = req.session;
 	if (!session.secret) session.secret = (Math.random()*Math.pow(36,5)|0).toString(36);
-	if (req.method==='POST' && req.param('secret')!==session.secret) {
-		console.log('Anti-csrf rejects this form. Session :', session);
-		res.send(403, "Your request can't be processed as it seems to be invalid");
-		return;
+	if (req.method==='POST') {
+		var url = req.protocol + '://' + req.get('host') + req.originalUrl;
+		if (req.param('secret')!==session.secret || url!=req.headers.referer) {
+			console.log('Anti-csrf rejects this form. Session :', url, session);
+			res.send(403, "There was a security problem, this request can't be processed.");
+			return;
+		}
 	}
 	res.locals.secret = session.secret;
 	console.log(res.locals);
