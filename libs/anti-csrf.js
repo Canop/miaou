631e1cde287c1d717c0bde4e84c4fcd53636ebb1
Canop <cano.petrole@gmail.com>
2014-06-08 09:13:05 +0200

    whitelisted the upload in csrf protection

--- libs/anti-csrf.js
+++ libs/anti-csrf.js
@@ -7,23 +7,30 @@
 // - insert in the forms a hidden parameter :
 //    input(type="hidden", name="secret", value=secret)
 
-module.exports = function(req, res, next){
-	var session = req.session;
-	if (!session.secret) session.secret = (Math.random()*Math.pow(36,5)|0).toString(36);
-	if (req.method==='POST') {
-		var refererHost = (req.headers.referer||'').match(/^https?:\/\/([^\/\:]+)/)[1];
-		if (req.param('secret')!==session.secret || refererHost!=req.host) {
-			console.log('Anti-csrf rejects this form');
-			console.log('req.host:', req.host);
-			console.log('referer:', req.headers.referer);
-			console.log('Session:', session);
-			res.send(403, "There was a security problem, this request can't be processed.");
-			return;
-		}
-	}
-	res.locals.secret = session.secret;
-	console.log(res.locals);
-	next();
-}
+module.exports = function(options){
 
+	var whitemap = {};
+	if (options && options.whitelist) {
+		options.whitelist.forEach(function(s){ whitemap[s]=1 })
+	}
+		
+	return function(req, res, next){
+		var session = req.session;
+		if (!session.secret) session.secret = (Math.random()*Math.pow(36,5)|0).toString(36);
+		if (req.method==='POST' && !whitemap[req.path]) {
+			var refererHost = (req.headers.referer||'').match(/^https?:\/\/([^\/\:]+)/)[1];
+			if (req.param('secret')!==session.secret || refererHost!=req.host) {
+				console.log('Anti-csrf rejects this form');
+				console.log('req.host:', req.host);
+				console.log('referer:', req.headers.referer);
+				console.log('Session:', session);
+				res.send(403, "There was a security problem, this request can't be processed.");
+				return;
+			}
+		}
+		res.locals.secret = session.secret;
+		console.log(res.locals);
+		next();
+	}
+}
 
