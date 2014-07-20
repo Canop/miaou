6b84798d3d30367aea34a420dd3de85ac5dd3ece
Canop <cano.petrole@gmail.com>
2014-07-20 10:23:18 +0200

    import of the new version of hu.js

--- src/client-scripts/hu.js
+++ src/client-scripts/hu.js
@@ -2,19 +2,20 @@
 //  website : http://Github.com/Canop/hu.js
 //  author  : denys.seguret@gmail.com
 ;(function(){
+
 	"use strict";
 	
-	var nextnum = 1,
+	var nn = 1, // counter for dynamically generated def id
 		U = function(n) { this.n = n },
 		fn = U.prototype,
 		nopx = { // css properties which don't need a unit
-			"columnCount": 1,
-			"fillOpacity": 1,
-			"flexGrow": 1,
-			"flexShrink": 1,
-			"fontWeight": 1,
-			"opacity": 1,
-			"zIndex": 1
+			columnCount: 1,
+			fillOpacity: 1,
+			flexGrow: 1,
+			flexShrink: 1,
+			fontWeight: 1,
+			opacity: 1,
+			zIndex: 1
 		};
 
 	function node(a, c){
@@ -25,7 +26,7 @@
 			if (m) {
 				var n = document.createElementNS("http://www.w3.org/2000/svg", m[1]);
 				if (/^svg$/i.test(n.tagName)) {
-					// this as a hack to force Firefox to see the dimension of the element
+					// hack to force Firefox to see the dimension of the element
 					ù('<rect', n).attr({width:"100%", height:"100%", opacity:0});
 				}
 				return n;
@@ -59,6 +60,8 @@
 		return this;
 	}
 
+	// removes the graphical nodes, not the defs
+	// (to remove everything, just call the standard DOM functions)
 	fn.empty = function(){
 		for (var l=this.n.childNodes, i=l.length; i--;) {
 			if (!/^defs$/i.test(l[i].tagName)) l[i].remove();
@@ -67,7 +70,7 @@
 	}
 
 	fn.autoid = function(){
-		return this.attrnv('id', 'ù'+nextnum++);
+		return this.attrnv('id', 'ù'+nn++);
 	}
 	
 	fn.text = function(s){
@@ -75,18 +78,18 @@
 		return this;
 	}
 	
+	// stores the passed element in the closest SVG parent of this
+	//  and gives it an automatic id.
 	fn.def = function(a){
 		var u = ù(a), p = this;
 		while (p) {
-			if (p.n.tagName==="svg") {
-				var defs = ù("defs", p);
-				if (!defs) defs = ù('<defs', p.n);
-				defs.n.appendChild(u.n); 
+			if (p.n.tagName === "svg") {
+				(ù("defs", p)||ù('<defs', p.n)).n.appendChild(u.n); 
 				return u.autoid();
 			}
 			p = ù(p.parentNode);
 		}
-		throw "Node not added to a SVG element";
+		throw "No parent SVG";
 	}
 
 	fn.stops = function(){
@@ -102,7 +105,7 @@
 			{offset:'100%', stopColor:c2}
 		);
 	}
-	
+
 	fn.width = function(v){
 		// window.getComputedStyle is the only thing that seems to work on FF when there are nested svg elements
 		if (v === undefined) return this.n.getBBox().width || parseInt(window.getComputedStyle(this.n).width); 
@@ -116,7 +119,7 @@
 	// css name value
 	fn.cssnv = function(name, value){
 		name = rcc(name);
-		if (value===undefined) return this.n.style[name];
+		if (value === undefined) return this.n.style[name];
 		if (typeof value === "number" && !nopx[name]) value += 'px';
 		this.n.style[name] = value;
 		return this;
@@ -124,17 +127,15 @@
 	
 	fn.css = function(a1, a2){
 		if (typeof a1 === "string") return this.cssnv(a1, a2);
-		for (var k in a1) {
-			this.cssnv(k, a1[k]);
-		}
+		for (var k in a1) this.cssnv(k, a1[k]);
 		return this;
 	}
 	
 	// attr name value
 	fn.attrnv = function(name, value){
 		name = rcc(name);
-		if (value===undefined) return this.n.getAttributeNS(null, name);
-		if (value instanceof U) value = "url(#"+value.attrnv('id')+")";
+		if (value === undefined) return this.n.getAttributeNS(null, name);
+		if (value instanceof U) value = "url(#"+value.n.id+")";
 		this.n.setAttributeNS(null, name, value);
 		return this;
 	}
@@ -164,36 +165,42 @@
 		return this;
 	}
 	
+	// dst is a map containing the destination css or attribute keys and values
 	fn.animate = function(dst, duration, cb){
 		var u = this, vars = [];
+		// the goal of this iteration is to build an array of objects for the animable properties, with
+		//  - v.k : the key
+		//  - v.f : the function used to set the style or attribute (fn.css or fn.attr)
+		//  - v.s : the start value
+		//  - v.e  : the end value
 		for (var k in dst){
 			var dstk = dst[k];
 			k = rcc(k);
-			var v = {key:k, dst:dstk},
+			var v = {k:k, e:dstk},
 				sk = this.n.style[k];
-			if (sk!==undefined && sk!=="") {
+			if (sk!==undefined && sk!=="") { // 0 or "0" would be ok
 				v.f = fn.css;
-				v.start = parseFloat(sk);
+				v.s = parseFloat(sk);
 			} else {
 				v.f = fn.attr;
 				var d = this.n[k] || this.attr(k);
 				if (d) {
-					v.start = parseFloat(d.baseVal ? d.baseVal.value : d); // you have a baseval for example in SVGAnimatedLength
+					v.s = parseFloat(d.baseVal ? d.baseVal.value : d); // you have a baseval for example in SVGAnimatedLength
 				} else {
-					v.start = 0;
+					v.s = 0;
 				}
 			}
 			vars.push(v);
 		}
-		var start = Date.now(), end = start+duration;
-		(function step(){
-			var now = Date.now()
+		var s = Date.now(), e = s+duration;
+		(function step(n){
+			n = Date.now()
 			vars.forEach(function(v){
-				v.f.call(u, v.key, v.start+(now-start)*(v.dst-v.start)/duration);
+				v.f.call(u, v.k, v.s+(n-s)*(v.e-v.s)/duration);
 			});
-			if (now<end) return setTimeout(step, 10);
+			if (n<e) return setTimeout(step, 10);
 			vars.forEach(function(v){
-				v.f.call(u, v.key, v.dst);
+				v.f.call(u, v.k, v.e);
 			});
 			if (cb) cb.call(u);
 		})();
