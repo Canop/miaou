7b250c3e13f84075875b9f260e2a0065c6ee8a82
Canop <cano.petrole@gmail.com>
2014-07-25 21:36:30 +0200

    changed the rendering of the ping buttons, removed the label

--- src/client-scripts/miaou.notify.js
+++ src/client-scripts/miaou.notify.js
@@ -3,6 +3,7 @@ var miaou = miaou || {};
 (function(){
 	var audio = new Audio('static/119472-ping-loud.wav'),
 		levels = ["none", "silent", "loud"],
+		labels = ["no ping",,];
 		level = localStorage['notification'];
 	if (!~levels.indexOf(level)) {
 		level = localStorage['notification'] = "none";
@@ -15,9 +16,9 @@ var miaou = miaou || {};
 	$(function(){
 		if (!$('body').hasClass('mobile')) { // desktop notifications don't seem to work on mobile
 			var $switch = $('<div id=ping>').addClass('radios').appendTo('#prefs');
-			$('<span>').addClass('label').text('ping :').appendTo($switch);
-			levels.forEach(function(l){
-				var $r = $('<span>').addClass('radio').text(l).click(function(){
+			$('<span>').addClass('label').text('').appendTo($switch);
+			levels.forEach(function(l, i){
+				var $r = $('<span>').addClass('radio').text(labels[i]||l).click(function(){
 					$(this).addClass('selected').siblings('.radio').removeClass('selected');
 					level = localStorage['notification'] = l;
 					if (level !== "none" && Notification.permission !== "granted") {
