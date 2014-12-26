f26957e33f6fb70972038a3e87ebb8f1ab4711a9
Canop <cano.petrole@gmail.com>
2014-12-26 11:23:33 +0100

    higlight ping message and remove ping when activating the tab

--- src/main-js/miaou.notif.js
+++ src/main-js/miaou.notif.js
@@ -5,12 +5,21 @@ miaou(function(notif, chat, horn, locals, md, ws){
 	//~ le ping concernant un message que tu as probablement vu (tu étais là peu avant ou la fenêtre était visible)
 	 //~ n'apparait pas tout de suite (il est dans un état non acquité mais non visible) et il est automatiquement
 	  //~ acquité si tu écris peu après, ou si tu réponds
-				
+	  
+	// when there's a ping in a new message and user is watching, highlight it
+					
 	// $m is a reference to the message element (useful when there's no message id)
 	var	notifications = [], // array of {r:roomId, rname:roomname, mid:messageid, $m:message}
 		notifMessage, // an object created with md.notificationMessage displaying notifications
 		nbUnseenMessages = 0,
 		lastUserAction = 0; // ms
+	
+	
+	function lastNotificationInRoom(){
+		for (var i=notifications.length; i--;) {
+			if (notifications[i].r==locals.room.id) return notifications[i];
+		}
+	}
 	
 	// called in case of user action proving he's right in front of the chat so
 	//  we should not ping him
@@ -137,6 +146,16 @@ miaou(function(notif, chat, horn, locals, md, ws){
 			if (vis()) {
 				nbUnseenMessages = 0;
 				notif.updateTab(0, 0);
+				// we go to the last notification message, highlight it and remove the ping
+				var ln = lastNotificationInRoom();
+				if (ln) {
+					var $md = ln.$md || $('#messages .message[mid='+ln.mid+']');
+					if ($md.length) {
+						md.goToMessageDiv($md);
+						ws.emit("rm_ping", ln.mid);
+						notif.removePing(ln.mid);
+					}
+				}
 				$('#input').focus();
 			}
 		});
