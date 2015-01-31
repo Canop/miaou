ceee8a90b7f62d3f1097322ecb2d295275da33f5
Canop <cano.petrole@gmail.com>
2015-01-31 23:30:42 +0100

    basic pad layout

--- src/page-js/pad.js
+++ src/page-js/pad.js
@@ -2,5 +2,21 @@
 miaou(function(locals){
 
 	console.log('in pad');
+	
+	var showroomstimer;
+	function showRoomsPanel(){
+		$('#room-and-rooms').addClass('open').removeClass('closed');
+		showroomstimer = setTimeout(function(){
+			$('#rooms').fadeIn("fast");
+		}, 500);
+	}
+	function hideRoomsPanel(){
+		clearTimeout(showroomstimer);
+		$('#rooms').hide();
+		$('#room-and-rooms').addClass('closed').removeClass('open');		
+	}
+	
+	$('#room-and-rooms').on('mouseenter', showRoomsPanel);
+	$('#stripe').on('mouseleave', hideRoomsPanel);
 
 });
