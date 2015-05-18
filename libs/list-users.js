04461d697afacef841af13ea0cce076fb6d0dd2c
Canop <cano.petrole@gmail.com>
2015-05-18 18:25:59 +0200

    Mention of @room in !!help !!li

--- libs/list-users.js
+++ libs/list-users.js
@@ -23,7 +23,8 @@ exports.registerCommands = function(registerCommand){
 	registerCommand({
 		name:'lu', fun:doCommand,
 		help:"list the users of the room",
-		detailedHelp:"list users having posted in the room or watching it"
+		detailedHelp:"list users having posted in the room or watching it.\n"+
+			"Those are the users receiving a `@room` message."
 	});
 }
 
