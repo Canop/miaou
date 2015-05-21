f16b8e24e089809c00310159e8cdd05055143563
Canop <cano.petrole@gmail.com>
2015-05-21 08:51:24 +0200

    DRYer JS code for the help

--- src/page-js/help.js
+++ src/page-js/help.js
@@ -1,10 +1,12 @@
 $(function(){
+	var h2hash;
 	$('h2, h3').each(function(){
 		var hash = '#' + $(this).text().replace(/\W+/g, '_');
 		if (hash==location.hash) {
 			$('html,body').scrollTop($(this).offset().top);
 		}
-		$(this).attr('hash',hash).clone().appendTo('#help-summary');
+		if (this.tagName==='H2') h2hash = hash;
+		$(this).attr('hash',hash).attr('h2hash',h2hash).clone().appendTo('#help-summary');
 	});
 	$('h1').click(function(){
 		location.hash = '';
@@ -18,31 +20,9 @@ $(function(){
 		$('html,body').animate({
 			scrollTop: $('#help-content [hash='+hash+']').offset().top
 		}, 500);
-	});
-	$('#help-summary h2').click(function(){
-		var h2 = this;
+		var h2hash = $(this).attr('h2hash');
 		$('#help-summary h3').each(function(){
-			if ($(this).is(':visible') || $(this).prevAll('h2').first()[0]!==h2) {
-				$(this).slideUp();
-			} else {
-				$(this).slideDown();
-			}
-		});
-	});
-	$('#help-content h2').click(function(){
-		var h2 = $('#help-summary [hash='+$(this).attr('hash')+']')[0];
-		$('#help-summary h3').each(function(){
-			if ($(this).prevAll('h2').first()[0]!==h2) {
-				$(this).slideUp();
-			} else {
-				$(this).slideDown();
-			}
-		});
-	});
-	$('#help-content h3').click(function(){
-		var h2 = $('#help-summary [hash='+$(this).attr('hash')+']').prevAll('h2').first()[0];
-		$('#help-summary h3').each(function(){
-			if ($(this).prevAll('h2').first()[0]!==h2) {
+			if ((h2hash===hash && $(this).is(':visible')) || $(this).attr('h2hash')!==h2hash) {
 				$(this).slideUp();
 			} else {
 				$(this).slideDown();
