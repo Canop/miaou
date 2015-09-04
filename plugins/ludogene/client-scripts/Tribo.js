4f2dc5faaf277385476ee2cdcd1d35ec464da169
Canop <cano.petrole@gmail.com>
2015-09-04 18:45:56 +0200

    fix Tribo not recognizing a 98-2 victory

--- plugins/ludogene/client-scripts/Tribo.js
+++ plugins/ludogene/client-scripts/Tribo.js
@@ -84,7 +84,7 @@ var Tribo = (function(){
 		//
 		computeZonesAndScores: function(g){
 			var nbmoves = g.moves.length;
-			if (nbmoves<7) return g.scores = [nbmoves+1>>1, nbmoves>>1];
+			if (nbmoves<6) return g.scores = [nbmoves+1>>1, nbmoves>>1];
 			g.cellZone = matrix(10, null); // holds a pointer to the zone containing the cell
 			var c = g.cells,
 				zones = g.zones = [],
