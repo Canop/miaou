1c0e4e7ef66ccb6fd29e7ee01db30558dd6bf2c9
Canop <cano.petrole@gmail.com>
2015-09-23 20:29:17 +0200

    Deletion of the old github-hook plugin
    which was replaced by scm-hook

--- plugins/github-hooks/sql/001-init_webhook_tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE github_hook (
-	repo text not null PRIMARY KEY,
-	nb_calls integer
-);
-CREATE TABLE github_hook_room (
-	repo text not null,
-	room integer references room(id),
-	PRIMARY KEY(repo, room)
-);
