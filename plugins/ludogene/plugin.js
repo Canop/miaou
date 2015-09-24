79c109bbb20aa789d812ec30026c8c4c6f6f5564
Canop <cano.petrole@gmail.com>
2015-09-24 22:29:08 +0200

    removed code used to fix old bug in game messages

--- plugins/ludogene/plugin.js
+++ plugins/ludogene/plugin.js
@@ -160,6 +160,7 @@ exports.registerGameObserver = function(type, cb){
 // This function is just on for a temporary time.
 // Its goal is to let the AI see games again when some of them
 // were forgotten
+/*
 exports.onSendMessage = function(shoe, m, send){
 	if (/^!!game /.test(m.content)) {
 		var match = m.content.match(/!!game @\S{3,} (.*)$/);
@@ -176,6 +177,7 @@ exports.onSendMessage = function(shoe, m, send){
 		}
 	}
 }
+*/
 
 // This function is just on for a temporary time.
 // Its goal is to cure messages containing games with the old saving format
