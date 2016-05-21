005bbede22293ba619f9a3e8c7e8b519fcb2bead
Canop <cano.petrole@gmail.com>
2016-05-21 22:40:08 +0200

    input autocomplete widget
    Nothing fancy. I just didn't find anything which was right
    and not ridiculously heavy

---
+++ src/main-js/autocomplete.js
@@ -0,0 +1,76 @@
+
+;$.fn.autocomplete = function(completer){
+	this.each(function(){
+		var	$field = $(this),
+			lastPat,
+			$menu = $("<div>").addClass("autocomplete-menu").hide().appendTo("body");
+		function receiveMatches(matches){
+			if (matches.length && !matches[0].startsWith($field.val())) {
+				console.log("disregard obsolete completion");
+				return;
+			}
+			if (!matches.length) return $menu.hide();
+			var offset = $field.offset();
+			$menu.css({
+				position: "fixed",
+				left: offset.left,
+				top: offset.top + $field.outerHeight(),
+				width: $field.width()
+			}).show().empty().append(matches.map(function(match){
+				return $("<div>").text(match);
+			}));
+		}
+		function select(s){
+			lastPat = s;
+			console.log("select", lastPat);
+			$field.val(lastPat);
+		}
+		function moveSelect(d){ // d: 1 or -1
+			var	$items = $menu.find("div"),
+				$selected = $menu.find(".selected"),
+				n = $items.length,
+				index;
+			if (!$selected.length) {
+				index = d>0 ? 0 : n-1;
+			} else {
+				index = ($selected.index()+d)%n;
+			}
+			$items.removeClass("selected");
+			if (index>=0) $items.eq(index).addClass("selected");
+		}
+		$menu.on("mousedown", "div", function(){
+			select(this.textContent);
+		}); // click comes after the blur, hence mousedown
+		$field
+		.on('keydown', function(e){
+			if ($menu.is(":visible")) {
+				console.log("ke:", e.which);
+				if (e.which==38) { // up arrow
+					moveSelect(-1);
+				} else if (e.which==40) { //down arrow
+					moveSelect(1);
+				} else if (e.which==27) { // escape
+					$menu.hide();
+					// revert to pre-enter state ?
+				} else if (e.which==13) { // enter
+					var s = $menu.find(".selected").text();
+					if (s) select(s);
+					$menu.hide();
+				} else {
+					return;
+				}
+				return false;
+			}
+		})
+		.on('keyup', function(e){
+			var pat = this.value.trim();
+			if (pat===lastPat) return;
+			if (pat) completer(this.value, receiveMatches);
+			else $menu.hide();
+			lastPat = pat;
+		})
+		.on('blur', function(){
+			$menu.hide();
+		});
+	});
+};
