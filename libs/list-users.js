ff4f195eedfad4d75c85dcf1cf062bca98545161
Canop <cano.petrole@gmail.com>
2016-08-30 13:16:01 +0200

    add the !!la command listing all room admins

--- libs/list-users.js
+++ libs/list-users.js
@@ -1,9 +1,24 @@
+// !!lu : list all room users (having rights or having posted)
+// !!la : list all room admins
 
 exports.configure = function(miaou){
 	return this;
 }
 
-function doCommand(ct){
+function la(ct){
+	return this.listRoomAuths(ct.shoe.room.id)
+	.filter(a => a.auth==="admin" || a.auth==="own")
+	.then(function(auths){
+		ct.nostore = true;
+		var lines = [
+			"Room admins:", "user|level", ":-:|:-:",
+			...auths.map(a => a.name + "|" + a.auth)
+		];
+		ct.reply(lines.join("\n"), true);
+	});
+}
+
+function lu(ct){
 	return this.listRoomUsers(ct.shoe.room.id).then(function(names){
 		ct.nostore = true;
 		ct.reply(
@@ -16,9 +31,14 @@ function doCommand(ct){
 
 exports.registerCommands = function(registerCommand){
 	registerCommand({
-		name:'lu', fun:doCommand,
-		help:"list the users of the room",
-		detailedHelp:"list users having posted in the room or watching it.\n"+
+		name: 'la', fun: la,
+		help: "list admins of the room",
+		detailedHelp: "list users having admin rights in this room."
+	});
+	registerCommand({
+		name: 'lu', fun: lu,
+		help: "list the users of the room",
+		detailedHelp: "list users having posted in the room or watching it.\n"+
 			"Those are the users receiving a `@room` message."
 	});
 }
