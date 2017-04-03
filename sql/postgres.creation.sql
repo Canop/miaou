c9846a1fc90fe25e932fc90ef9e0654ae23016cc
Canop <cano.petrole@gmail.com>
2017-04-03 22:31:28 +0200

    fix crash at install due to bad version in sql script

--- sql/postgres.creation.sql
+++ sql/postgres.creation.sql
@@ -2,7 +2,7 @@ CREATE TABLE db_version (
 	component varchar(30) primary key,
 	version integer NOT NULL
 );
-insert into db_version (component, version) values('core', 22);
+insert into db_version (component, version) values('core', 24);
 
 CREATE TABLE room (
 	id serial primary key,
