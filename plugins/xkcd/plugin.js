0427b68f991e641698cdb83cec40b916ac9731d7
Canop <cano.petrole@gmail.com>
2017-08-07 21:03:43 +0200

    various minor style fixes

--- plugins/xkcd/plugin.js
+++ plugins/xkcd/plugin.js
@@ -2,12 +2,12 @@
 // box xkcd links
 
 function abstract($, line){
-	var xplain_base = 'http://www.explainxkcd.com/wiki/index.php/',
+	var	explainBase = 'https://www.explainxkcd.com/wiki/index.php/',
 		$box = $('<div/>').addClass('xkcd'),
 		$abstract = $('<div/>').addClass('abstract'),
 		$comic = $('#middleContainer'),
 		[permaLink, id] = $('#middleContainer').text().match(
-			"Permanent link to this comic: (https://xkcd.com/([0-9]+))/"
+			/Permanent link to this comic: (https:\/\/xkcd.com\/([0-9]+))/
 		).slice(1, 3);
 	if (!$comic.length) {
 		return null;
@@ -18,23 +18,23 @@ function abstract($, line){
 			"XKCD: " + $("#ctitle").text()
 		)
 	));
-	$abstract.append($("<p>").append($('#comic img')));
+	$abstract.append($("<p>").append('#comic img'));
 	$abstract.append(
 		$("<a>")
-			.attr("href", xplain_base+id )
-			.attr("target", "_blank")
-			.text("Explain xkcd")
-			.attr("title", "It's 'cause you're dumb.")
-			.addClass("xkcd_link")
+		.attr("href", explainBase+id )
+		.attr("target", "_blank")
+		.text("Explain xkcd")
+		.attr("title", "It's 'cause you're dumb.")
+		.addClass("xkcd-link")
 	);
-	
+
 	return $('<div>').append($box).html();
 }
 
 exports.init = function(miaou){
 	miaou.lib("page-boxers").register({
 		name: "xkcd",
-		pattern:/^\s*https?:\/\/(www\.)?xkcd\.com\/[0-9]*\/?\s*$/,
-		box:abstract
+		pattern: /^\s*https?:\/\/(www\.)?xkcd\.com\/[0-9]*\/?\s*$/,
+		box: abstract
 	});
 }
