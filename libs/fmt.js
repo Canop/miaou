e888fd5ff6fcab0c182d3cf2e7304e3204c46a6f
Canop <cano.petrole@gmail.com>
2017-10-26 21:29:33 +0200

    fmt function to build lists like "a, b, c, and d"
    I always have to write it everywhere...

--- libs/fmt.js
+++ libs/fmt.js
@@ -69,3 +69,13 @@ exports.playerLink = function(name){
 exports.roomLink = function(room){
 	return "["+naming.makeMarkdownCompatible(room.name)+"]("+room.id+"#)";
 }
+
+// make a list in the Oxford comma style (eg "a, b, c, and d")
+exports.oxford = function(arr, ifempty){
+	var l = arr.length;
+	if (!l) return ifempty;
+	if (l<2) return arr[0];
+	arr = arr.slice();
+	arr[l-1] = "and " + arr[l-1];
+	return arr.join(", ");
+}
