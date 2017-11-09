ccadaa064914f2c7fc06fb3f01cc35c41064ee6f
Canop <cano.petrole@gmail.com>
2017-11-09 19:57:05 +0100

    optional features of the server shown to the client

--- libs/chat.js
+++ libs/chat.js
@@ -1,5 +1,4 @@
-const	Promise = require("bluebird"),
-	fs = require('fs'),
+const	fs = require('fs'),
 	path = require('path'),
 	auths = require('./auths.js'),
 	prefs = require('./prefs.js'),
@@ -7,6 +6,7 @@ const	Promise = require("bluebird"),
 
 var	clientSidePluginNames,
 	accessRequestPlugins,
+	features,
 	db;
 
 exports.configure = function(miaou){
@@ -14,67 +14,58 @@ exports.configure = function(miaou){
 		return fs.existsSync(path.resolve(__dirname, '..', n, '..', 'client-scripts'))
 	}).map(p=>p.split('/').slice(-2, -1)[0]);
 	accessRequestPlugins = miaou.plugins.filter(p => p.beforeAccessRequest);
+	features = {
+		search: {
+			regularExpressions: miaou.conf("search", "regularExpressions"),
+			exactExpressions: miaou.conf("search", "exactExpressions")
+		}
+	};
 	db = miaou.db;
 	return this;
 }
 
 exports.appGet = function(req, res){
-	var	roomId = +req.params[0],
+	let	roomId = +req.params[0],
 		userId = req.user.id;
 	if (!roomId) {
 		// not an error as it happens when there's no room id in url
 		res.redirect(server.url('/rooms'));
 		return;
 	}
-	db.on()
-	.then(function(){
-		return [
-			this.fetchRoomAndUserAuth(roomId, userId),
-			this.getRoomUserActiveBan(roomId, userId),
-			prefs.get.call(this, userId)
-		]
-	})
-	.spread(function(room, ban, userPrefs){
+	db.do(async function(con){
+		let room = await con.fetchRoomAndUserAuth(roomId, userId);
+		if (!room) throw new Error("No room with id " + roomId);
 		room.path = server.roomPath(room);
 		req.session.room = room;
-		var	isMobile = server.mobile(req),
-			theme = prefs.theme(userPrefs, req.query.theme, isMobile);
+		let ban = await con.getRoomUserActiveBan(roomId, userId);
 		if (ban || (room.private && !auths.checkAtLeast(room.auth, 'write'))) {
-			if (room.dialog) {
-				return server.renderErr(res, "You can't enter this room");
+			let lastAccessRequest = await con.getLastAccessRequest(roomId, userId);
+			let args = {
+				vars: { room },
+				room,
+				lastAccessRequest,
+				theme,
+				canQueryAccess: true,
+				specificMessage: null
+			};
+			for (let i=0; i<accessRequestPlugins.length; i++) {
+				args = await accessRequestPlugins.beforeAccessRequest(args, req.user);
 			}
-			return this.getLastAccessRequest(room.id, req.user.id)
-			.then(function(ar){
-				var args = {
-					vars: { room },
-					room,
-					lastAccessRequest:ar,
-					theme,
-					canQueryAccess: true,
-					specificMessage: null
-				};
-				return Promise.reduce(accessRequestPlugins, function(args, p){
-					return p.beforeAccessRequest(args, req.user);
-				}, args);
-			})
-			.then(function(args){
-				res.render('request.pug', args);
-			});
+			res.render("request.pug", args);
+			return;
 		}
-		var locals = {
+		let	userPrefs = await prefs.get.call(con, userId),
+			isMobile = server.mobile(req),
+			theme = prefs.theme(userPrefs, req.query.theme, isMobile);
+		let locals = {
 			me: req.user,
 			room,
 			userPrefs,
+			features,
 			pluginsToStart: clientSidePluginNames
 		};
 		res.render(isMobile ? 'pad.mob.pug' : 'pad.pug', {vars:locals, theme});
-	})
-	.catch(db.NoRowError, function(){
-		console.log("no room found for id", roomId);
-		res.redirect(server.url('/rooms'));
-	})
-	.catch(function(err){
+	}, function(err){
 		server.renderErr(res, err);
-	})
-	.finally(db.off);
+	});
 }
