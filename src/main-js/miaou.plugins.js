1e92cdc81b862d8698c5eee5517a27b5791fed89
Canop <cano.petrole@gmail.com>
2017-11-29 23:05:45 +0100

    explicit API for adding the client side part(s) of a plugin

--- src/main-js/miaou.plugins.js
+++ src/main-js/miaou.plugins.js
@@ -1,4 +1,5 @@
 miaou(function(plugins, locals){
+
 	plugins.start = function(){
 		locals.pluginsToStart.forEach(function(name){
 			if (!plugins[name]) {
@@ -7,5 +8,14 @@ miaou(function(plugins, locals){
 			}
 			plugins[name].start();
 		});
+	}
+
+	// add the client part of a plugin (or part of this part)
+	// If called several times for the same name, object properties will be merged.
+	// This should be the prefered way to add a plugin. Doing plugins[name] = ... directly
+	//  is deprecated and will be removed
+	plugins.add = function(name, obj){
+		if (!plugins[name]) plugins[name] = Object.create(null);
+		for (var key in obj) plugins[name][key] = obj[key];
 	}
 });
