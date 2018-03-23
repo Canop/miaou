3f41797bdcdbb9609bfce95cca874070f12fbb13
Canop <cano.petrole@gmail.com>
2018-03-23 20:54:55 +0100

    !!webshot command accepts virtual browser dimensions

--- plugins/webshot/plugin.js
+++ plugins/webshot/plugin.js
@@ -13,21 +13,31 @@ exports.init = function(miaou){
 }
 
 async function onCommand(ct){
-	const match = ct.args.match(/https?:\/\/\S+/);
-	if (!match) throw new Error("This command needs an URL as argument");
-	let bo = bench.start("webshot");
-	const url = match[0];
+	const urlMatch = ct.args.match(/\bhttps?:\/\/\S+/);
+	if (!urlMatch) throw new Error("This command needs an URL as argument");
+	const url = urlMatch[0];
 	console.log('url:', url);
+	let width = WIDTH;
+	let height = HEIGHT;
+	const sizeMatch = ct.args.match(/\b(\d+)\s*(?:x|\*)\s*(\d+)\b/);
+	if (sizeMatch) {
+		width = sizeMatch[1];
+		height = sizeMatch[2];
+		if (width<10) throw new Error("invalid width");
+		if (height<10) throw new Error("invalid height");
+		if (height*width>3000*2000) throw new Error("requested size too big");
+	}
 	const instance = await phantom.create();
+	let bo = bench.start("webshot");
 	const page = await instance.createPage();
-	await page.property('viewportSize', { width: WIDTH, height: HEIGHT });
+	await page.property('viewportSize', {width, height});
 	const status = await page.open(url);
 	console.log('status:', status);
 	let b64 = await page.renderBase64("png");
 	await instance.exit();
 	let data = await sendToImgur(b64);
 	console.log('data:', data);
-	if (!data.link) { // should not happen, I think (because handled in upload.js
+	if (!data.link) { // should not happen, I think (because handled in "upload" lib
 		if (data.error) {
 			throw new Error("Imgur sent an error: " + data.error);
 		} else {
@@ -42,7 +52,9 @@ exports.registerCommands = function(registerCommand){
 	registerCommand({
 		name:'webshot',
 		fun:onCommand,
-		help:"takes a screenshot of a website. Example: `!!webshot https://dystroy.org`"
+		help:"takes a screenshot of a website. Example: `!!webshot https://dystroy.org`",
+		detailedHelp:"You can also precise the dimension of the virtual browser:"
+			+ "\n* `!!!!webshot https://dystroy.org 200x3000`"
 	});
 }
 
