0535eeceb1d6fa1fc290972b3dace7cff807de83
Canop <cano.petrole@gmail.com>
2018-05-04 20:34:08 +0200

    prettier graph
    - theme color for the text
    - no more white background (thanks to the normal theme text color)
    - all bars now the same size
    - scrollbar if necessary (instead of infinite shrinking)
    - miaou bubble instead of svg bubble (so the bubble can be greater)
    - fix hover detection glitches (due to pointer events)
    - hovered x value is shown (background change)

--- plugins/graph/client-scripts/graph-renderer.js
+++ plugins/graph/client-scripts/graph-renderer.js
@@ -55,6 +55,10 @@ miaou(function(md, plugins){
 		return b>a ? b : a;
 	}
 
+	function legend(color){
+		return `<i class=graph-legend style="background:${color}"></i>`;
+	}
+
 	var nm3 = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
 	var m3 = {};
 	for (var i=0; i<nm3.length; i++) m3[nm3[i]]=i+1;
@@ -89,15 +93,9 @@ miaou(function(md, plugins){
 		},
 	];
 	var colors = [
-		'#e9967a', 'rgba(0,143,143,.4)', 'rgba(251, 170, 5, .4)',
-		'rgba(251, 237, 5, .5)', 'rgba(188,143,143,0.35)',
-		'rgba(180, 237, 228, .45)', 'rgba(192, 169, 244, .45)', 'rgba(160, 5, 5, .3)',
-		'rgba(176,224,230,0.32)', 'rgba(255,255,102,0.3)', 'rgba(255,35,215,0.3)'
+		'#e9967a', '#9cd3d3', '#e91e63', '#795548', '#8bc34a', '#00bcd4',
+		'#ffc107', '#4e5050', '#317334', '#7126b1', '#ce0d08', '#959e75'
 	];
-	var pop;
-	function hidePop(){
-		if (pop) pop.remove();
-	}
 
 	function render($c, m){
 		if (!m.content) return;
@@ -143,29 +141,30 @@ miaou(function(md, plugins){
 		}
 
 		var	xvals = xcol.vals,
+			availableWidth = Math.max($c.width()-120, 300),
 			maxXLabelLength = Math.max.apply(0, xvals.map(function(xv){ return xv.label.length })),
 			rotateXLabels = maxXLabelLength > 5,
 			mt = 2, // margin top
 			mr = 5, // margin right
 			mb = Math.max(58, 27+16*nbycols), // margin bottom
 			ml = rotateXLabels ? 35 : 5, // margin left
+			bm = 1, // space between two bars and between a bar and the border of its xval
 			n = xvals.length,
-			g = ù('<svg', $c[0]).css({ height:H, width:800 }),
-			gW = Math.max(50, Math.min(g.width()-mr-ml, 40*n*ycols.length)),
+			nbbars = n * nbycols,
+			barWidth = Math.min(Math.max(6, availableWidth/nbbars-bm|0), 20),
+			xWidth = ((barWidth+bm)*nbycols+bm), // width of a xval
+			gW = xWidth*n,
 			W = gW+mr+ml,
-			xmin = xvals[0].start,
+			g = ù('<svg', $c[0]).css({ height:H, width:W }),
 			miny,
 			maxy,
+			h = H - mb - mt,
 			minsumy = 0,
-			maxsumy = 0,
-			w = W-ml-mr, h = H-mt-mb,
-			rx = w / (xvals[n-1].end-xvals[0].start);
+			maxsumy = 0;
 
 		ycols.forEach(function(ycol, j){
 			ycol.color = colors[j%colors.length];
-			$table.find("th").eq(ycol.i).append(
-				$("<i class=graph-legend>").css("background", ycol.color)
-			);
+			$table.find("th").eq(ycol.i).append(legend(ycol.color));
 			ycol.minsum = 0;
 			ycol.maxsum = 0;
 			var sum = 0;
@@ -178,7 +177,7 @@ miaou(function(md, plugins){
 				ycol.min = min(v, ycol.min);
 				ycol.max = max(v, ycol.max);
 			}
-			
+
 			if (ycol.min>0 && ycol.min<.9*ycol.max) ycol.min = 0;
 			miny = min(miny, ycol.min);
 			maxy = max(maxy, ycol.max);
@@ -201,10 +200,19 @@ miaou(function(md, plugins){
 		});
 
 		xvals.forEach(function(xval, i){
-			var	x1 = Math.floor(ml+(xvals[i].start-xmin)*rx)+2,
-				x2 = ml+(xvals[i].end-xmin)*rx-3,
-				xm = (x1+x2)/2,
-				barWidth = Math.floor((x2-x1-5)/nbycols);
+			var	x1 = ml + i*xWidth,
+				x2 = x1 + xWidth,
+				xm = (x1+x2)/2;
+			var rect = ù('<rect', g).attr({
+				class: "xval",
+				x:x1, width:xWidth-1, y:0, height:h+mt, fill:"transparent", cursor:'crosshair'
+			});
+			$(rect.n).bubbleOn({
+				html: [
+					`<div class=bubble-title>${xval.label}</div>`,
+					...ycols.map(c => `${legend(c.color)} ${c.name} : ${c.rawvals[i]}`)
+				].join("<br>")
+			});
 			ycols.forEach(function(ycol, j){
 				var	val = ycol.vals[i],
 					y;
@@ -215,11 +223,11 @@ miaou(function(md, plugins){
 					y = mt + h - Math.floor((val-ycol.min)*ycol.r);
 				}
 				var	height = Math.ceil(h-y+mt),
-					xbar = x1+(barWidth+2)*j+4;
+					xbar = x1+(barWidth+bm)*j;
 				if (height) {
 					ù('<rect', g).attr({
 						x:xbar, y:y, width:barWidth, height:height,
-						fill:ycol.color
+						class: "bar", fill:ycol.color
 					});
 				}
 				if (options.sum||options["sum"+j]) {
@@ -234,45 +242,11 @@ miaou(function(md, plugins){
 			var	x = xm,
 				y = h+mt+10;
 			var text = ù('<text', g).text(xval.label).attr({
-				x:x, y:y, alignmentBaseline:"middle", fontSize:"85%", opacity:.9
+				class: "label", x:x, y:y, alignmentBaseline:"middle", fontSize:"85%", opacity:.9
 			});
 			if (rotateXLabels) {
 				text.attr({textAnchor:"end", transform:"rotate(-45 "+x+" "+y+")"});
 			}
-			var showPop = function(){
-				var	bubbleHeight = nbycols*16+20,
-					l = 80;
-				if (xm-l<10) l = 10;
-				else if (xm+l>W-10) l = 135;
-				var path = "M "+(xm)+" "+(mt+h-5.5)
-					+ " l 10 10"
-					+ " h "+(140-l)
-					+ " v "+bubbleHeight
-					+ " h -160"
-					+ " v-"+bubbleHeight
-					+ " h "+l
-					+ " l 10 -10";
-				var xmr = xm + 70 - l;
-				pop = ù("<g", g).attr({
-					alignmentBaseline:"middle", fontSize:"85%"
-				});
-				ù('<path', pop).attr({d:path, fill:"#eee", stroke:"#aaa", strokeWidth:1});
-				ù('<text', pop).text(xval.label).attr({
-					x:xmr, y:mt+h+19, textAnchor:"middle"
-				});
-				ycols.forEach(function(ycol, j){
-					ù('<rect', pop).attr({
-						x:xmr-70, width:6, y:mt+h+28+j*16, height:6,
-						fill:ycol.color
-					});
-					ù('<text', pop).text(ycol.name+": "+ycol.rawvals[i]).attr({
-						x:xmr-58, y:mt+h+35+j*16
-					});
-				});
-			}
-			ù('<rect', g).attr({
-				x:x1, width:x2-x1, y:0, height:h, fill:"transparent", cursor:'crosshair'
-			}).on('mouseenter', showPop).on('mouseleave', hidePop);
 			ù('<line', g).attr({
 				x1:x1-.5, x2:x1-.5, y1:mt+h-2, y2:mt+h+1,
 				stroke:"#666", strokeWidth:1
@@ -288,8 +262,12 @@ miaou(function(md, plugins){
 		});
 
 		var $tablewrap = $table.closest('.tablewrap');
-		$('<div>').addClass('graph-tbl-wrapper').insertBefore($tablewrap)
-		.append($tablewrap).append(g.n);
+
+		var $wrapper = $("<div class=graph-wrapper>").append(g.n).insertBefore($tablewrap);
+		setTimeout(function(){
+			$wrapper.scrollLeft(W)
+		}, 10);
+
 	}
 
 	plugins.graph = {
