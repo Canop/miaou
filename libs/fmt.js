25fd0f159b9107d39156faae619bcafa3afc6d71
Canop <cano.petrole@gmail.com>
2018-05-14 21:53:07 +0200

    add two formating functions

--- libs/fmt.js
+++ libs/fmt.js
@@ -11,13 +11,13 @@ exports.date = function(seconds, pat){
 	var	date = new Date(seconds*1000),
 		month = date.getMonth();
 	return pat
-		.replace(/DD/g, td(date.getDate()))
-		.replace(/MMM/g, MMM[date.getMonth()])
-		.replace(/MM/g, td(month+1))
-		.replace(/YYYY/g, date.getFullYear())
-		.replace(/YY/g, date.getYear())
-		.replace(/hh/g, td(date.getHours()))
-		.replace(/mm/g, td(date.getMinutes()));
+	.replace(/DD/g, td(date.getDate()))
+	.replace(/MMM/g, MMM[date.getMonth()])
+	.replace(/MM/g, td(month+1))
+	.replace(/YYYY/g, date.getFullYear())
+	.replace(/YY/g, date.getYear())
+	.replace(/hh/g, td(date.getHours()))
+	.replace(/mm/g, td(date.getMinutes()));
 }
 
 // formats a duration in milliseconds (ex: " 08h 37m 27s")
@@ -29,6 +29,41 @@ exports.duration = function(t){
 exports.int = function(num){
 	if (!+num) return num || ' ';
 	return (+num).toString().replace(/\B(?=(\d{3})+(?!\d))/g, "\u2009");
+}
+
+// Formate un nombre. La précision après la virgule est optionnelle (valeur par défaut : 2)
+// exemples :
+//   formatFloat(100.0)	 => "100"
+//   formatFloat(100.0, 12) => "100"
+//   formatFloat(100.1)	=> "100.1"
+//   formatFloat(100.1, 0) => "100"
+//   formatFloat(100.7, 0) => "101"
+//   formatFloat(.0434) => "0.043"
+//   formatFloat(1.999999) => "2"
+//   formatFloat(.0000047) => "0"
+//   formatFloat(.0000047, 6)  => "0.000005"
+//   formatFloat(.0000047, 12) => "0.0000047"
+//   formatFloat(undefined) => ""
+//   formatFloat(NaN) => ""
+exports.float = function(v, p=2){
+	if (!isFinite(v)) return '';
+	if (p<=0) return ''+Math.round(v);
+	return v.toFixed(p||2).replace(/(\.0+$)|(0+$)/, '');
+}
+
+// format a number as a storage size
+// exemples:
+// 	bytes(7477395456) => "6.96 GB"
+// 	bytes(7477395456, 1) => "7 GB"
+// 	bytes(7477395456, 3) => "6.964 GB"
+// 	bytes(747739) => "730.21 KB"
+exports.bytes = function(v, p){
+	v = +v;
+	if (v<=0) return '0 Byte';
+	let	k = 1024,
+		sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+		i = Math.floor(Math.log(v) / Math.log(k));
+	return exports.float(v / Math.pow(k, i), p) + ' ' + sizes[i];
 }
 
 const alignMd = { l: ":-", c: ":-:", r: "-:" };
