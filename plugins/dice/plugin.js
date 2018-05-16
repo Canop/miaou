f318f60abeedd345ed5db790fbbba5f68c1c6270
Canop <cano.petrole@gmail.com>
2018-05-16 22:27:13 +0200

    !!dice now takes options all|roll|distribution
    default is distribution, which can be shortened into dist

--- plugins/dice/plugin.js
+++ plugins/dice/plugin.js
@@ -11,86 +11,117 @@ const operators = {
 };
 
 let fmt;
+let cmdArgRegex;
 
 exports.init = async function(miaou){
 	fmt = miaou.lib("fmt");
+	cmdArgRegex = miaou.lib("rex")`
+		([a-z ]*)			// options (optional)
+		(\d*\s*d\s*[\d+-]+)		// left rolldef (mandatory)
+		(?:				// inequation (optional)
+			\s*
+			([<>=]+)		// operator
+			\s*
+			([\w+-]+)		// right rolldef or scalar
+		)?
+		\s*$
+		/i`;
 }
 
-function showDef(def, wantRoll=false, wantAllDice=false, wantDistribution=false){
-	if (def.N>5000) wantRoll = false;
-	if (def.N>20) wantAllDice = false;
-	if (def.N<2 || def.N*def.S>5000) wantDistribution = false;
+function showDef(def, options){
 	let md = def.description();
 	md += `, expecting **${def.expect()}**`;
-	if (wantAllDice) {
-		let roll = def.roll();
-		md += `\nRoll: **${roll.result}**`;
-		md += "\n" + fmt.tbl({
-			cols: roll.dice.map((_, i) => i+1),
-			rows: [roll.dice]
-		});
-	} else if (wantRoll) {
-		md += `\nRoll: **${def.sum()}**`
+	if (options.roll) {
+		if (def.N>50000) throw new Error("Too Many Dice : Board is flooded");
+		if (def.N<=30) {
+			let roll = def.roll();
+			md += `\nRoll: **${roll.result}**`;
+			md += "\n" + fmt.tbl({
+				cols: roll.dice.map((_, i) => i+1),
+				rows: [roll.dice]
+			});
+		} else {
+			md += `\nRoll: **${def.sum()}**`
+		}
 	}
-	if (wantDistribution) {
+	if (options.distrib && def.N>1 && def.N*def.S<10000) {
 		md += "\n## Distribution:\n" + def.distribution().md();
 	}
 	return md;
 }
 
-function showDefScalar(def, operator, scalar){
+function showDefScalar(def, operator, scalar, options){
 	let md = def.description();
 	let p = def.distribution().compareToScalar(operator, scalar);
 	md += `\nProbability to have ${def.str()} ${operator.name} ${scalar} : **${fmt.float(p*100)}%**`;
-	if (def.N>1 && def.N*def.S<=5000) {
+	if (options.roll) {
+		let sum = def.sum();
+		md += `\nRoll: ${sum} => **${operator(sum, scalar) ? "yes" : "no"}**`
+	}
+	if (options.distrib && def.N>1 && def.N*def.S<=5000) {
 		md += "\n## Distribution:\n" + def.distribution().md();
 	}
 	return md;
 }
 
-function showDefDef(defA, operator, defB){
+function showDefDef(defA, operator, defB, options){
 	let md = "";
 	let distA = defA.distribution();
 	let distB = defB.distribution();
 	let p = distA.compareToDistribution(operator, distB);
 	md += `\nProbability to have ${defA.str()} ${operator.name} ${defB.str()} : **${fmt.float(p*100)}%**`;
-	let min = Math.min(distA.minPossibleValue(), distB.minPossibleValue());
-	let max = Math.max(distA.maxPossibleValue(), distB.maxPossibleValue());
-	if (max-min<=500) {
-		md += "\n## Distribution:\n";
-		let rows = [];
-		for (let v=min; v<=max; v++) {
-			let pa = distA.probability(v);
-			let pb = distB.probability(v);
-			if (pa+pb<10**-10) continue;
-			rows.push([v, `${fmt.float(pa*100)}%`, `${fmt.float(pb*100)}%`]);
-		}
-		md += "#graph(hideTable,compare)\n" + fmt.tbl({
-			cols: ["value", `proba(${defA.str()})`, `proba(${defB.str()})`],
-			aligns: "rcc",
-			rows
+	if (options.roll) {
+		let sa = defA.sum();
+		let sb = defB.sum();
+		md += "\n## Roll:";
+		md += "\n" + fmt.tbl({
+			cols: [ defA.str(), operator.name, defB.str() ],
+			rows: [[ sa, `**${operator(sa, sb) ? "yes" : "no"}**`, sb ]]
 		});
+	}
+	if (options.distrib && defA.N>1 && defB.N>1) {
+		let min = Math.min(distA.minPossibleValue(), distB.minPossibleValue());
+		let max = Math.max(distA.maxPossibleValue(), distB.maxPossibleValue());
+		if (max-min<=500) {
+			md += "\n## Distribution:\n";
+			let rows = [];
+			for (let v=min; v<=max; v++) {
+				let pa = distA.probability(v);
+				let pb = distB.probability(v);
+				if (pa+pb<10**-10) continue;
+				rows.push([v, `${fmt.float(pa*100)}%`, `${fmt.float(pb*100)}%`]);
+			}
+			md += "#graph(hideTable,compare)\n" + fmt.tbl({
+				cols: ["value", `proba(${defA.str()})`, `proba(${defB.str()})`],
+				aligns: "rcc",
+				rows
+			});
+		}
 	}
 	return md;
 }
 
 function onCommand(ct){
-	let match = ct.args.match(/^\s*(\d*\s*d\s*[\d+-]+)(?:\s*([<>=]+)\s*([\w+-]+))?\s*$/i);
+	let match = ct.args.match(cmdArgRegex);
 	if (!match) throw new Error("Invalid command");
-	let [, left, op, right] = match;
+	let [, soptions, left, op, right] = match;
+	let options = {};
+	options.all = /\ball\b/.test(soptions);
+	options.roll = options.all || /\broll\b/.test(soptions);
+	options.distrib = options.all || !options.roll || /\bdist/.test(soptions);
 	let leftDef = new DiceRollDefinition(left);
 	let md;
 	if (op) {
 		let operator = operators[op];
 		if (!operator) throw new Error("Unknown Operator: "+op);
 		if (right==+right) {
-			md = showDefScalar(leftDef, operator, +right);
+			md = showDefScalar(leftDef, operator, +right, options);
 		} else {
 			let rightDef = new DiceRollDefinition(right);
-			md = showDefDef(leftDef, operator, rightDef);
+			md = showDefDef(leftDef, operator, rightDef, options);
 		}
 	} else {
-		md = showDef(leftDef, true, true, true);
+		md = showDef(leftDef, options);
 	}
 	let duration = ct.end();
 	md += `\n*duration: ${duration}µs*`;
