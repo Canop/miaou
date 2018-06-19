a9dd60598d19c51acd69dc3e9c8ddb7e848a9106
Canop <cano.petrole@gmail.com>
2018-06-19 19:57:00 +0200

    auths: from explicit promises to async/await

--- libs/auths.js
+++ libs/auths.js
@@ -5,7 +5,7 @@ const	levels = ['read', 'write', 'admin', 'own'],
 	server = require('./server.js'),
 	ws = require('./ws.js');
 
-var	serverAdminIds,
+let	serverAdminIds,
 	miaou,
 	db;
 
@@ -30,28 +30,26 @@ exports.init = async function(){ // make miaou.lib async to avoid this ?
 }
 
 function startPeriodicAccessRequestCleaning(miaou){
-	var maxAge = miaou.conf("cleaning-frequencies", "old-access-requests") || 2*60*60; // 2 hours
-	var checkInterval = Math.max(10, Math.min(1000, maxAge/5|0));
+	let maxAge = miaou.conf("cleaning-frequencies", "old-access-requests") || 2*60*60; // 2 hours
+	let checkInterval = Math.max(10, Math.min(1000, maxAge/5|0));
 	setInterval(function(){
 		console.log("checking access requests");
-		db.on().then(function(){
-			var now = Date.now()/1000|0;
-			return this.execute(
+		db.do(async function(con){
+			let now = Date.now()/1000|0;
+			let res = await con.execute(
 				"update access_request set denied=$1, deny_message=$2"
 				+ " where denied is null and requested<$3",
 				[now, "Access Request Too Old - Automatic Deletion", now-maxAge],
 				"purge_old_access_requests"
 			);
-		}).then(function(res){
 			console.log("Removed", res.rowCount, "old access request(s)");
-		}).finally(db.off);
-
+		});
 	}, checkInterval*1000);
 }
 
 
 exports.checkAtLeast = function(auth, neededAuth){
-	for (var i=levels.length; i-->0;) {
+	for (let i=levels.length; i-->0;) {
 		if (levels[i]===auth) return true;
 		if (levels[i]===neededAuth) return false;
 	}
@@ -64,19 +62,20 @@ exports.isServerAdmin = function(user){
 
 // handles GET of the auths /page
 exports.appGetAuths = function(req, res){
-	db.on([+req.query.id, +req.user.id])
-	.spread(db.fetchRoomAndUserAuth)
-	.then(function(room){
+	db.do(async function(con){
+		let room;
+		try {
+			room = await con.fetchRoomAndUserAuth(+req.query.id, +req.user.id);
+		} catch (err) {
+			return server.renderErr(res, "room not found");
+		}
 		room.path = server.roomPath(room);
-		return [
-			this.listRoomAuths(room.id),
-			this.listOpenAccessRequests(room.id),
-			this.listRecentUsers(room.id, 50),
-			this.listActiveBans(room.id),
-			room
-		];
-	}).spread(function(auths, requests, recentUsers, bans, room){
-		var dontlistasrecent = {}, unauthorizedUsers = [];
+		let auths = await con.listRoomAuths(room.id);
+		let requests = await con.listOpenAccessRequests(room.id);
+		let recentUsers = await con.listRecentUsers(room.id, 50);
+		let bans = await con.listActiveBans(room.id);
+		let dontlistasrecent = {};
+		let unauthorizedUsers = [];
 		auths.concat(requests).forEach(function(a){
 			dontlistasrecent[a.player] = true;
 		});
@@ -84,38 +83,41 @@ exports.appGetAuths = function(req, res){
 			if (!dontlistasrecent[u.id]) unauthorizedUsers.push(u);
 		});
 		res.render('auths.pug', {
-			vars:{room},
-			room, auths, requests, unauthorizedUsers, bans:bans
+			vars: {room},
+			room, auths, requests, unauthorizedUsers, bans
 		});
-	}).catch(db.NoRowError, function(){
-		server.renderErr(res, "room not found");
-	}).catch(function(err){
+	}, function(err){
 		server.renderErr(res, err);
-	}).finally(db.off);
+	});
 }
 
 // handles POST of the auths /page
 exports.appPostAuths = function(req, res){
-	var room;
-	db.on([+req.query.id, +req.user.id])
-	.spread(db.fetchRoomAndUserAuth)
-	.then(async function(r){
-		room = r;
-		var	author = req.user.name,
+	db.do(async function(con){
+		let room;
+		try {
+			room = await con.fetchRoomAndUserAuth(+req.query.id, +req.user.id);
+		} catch (err) {
+			return server.renderErr(res, "room not found");
+		}
+		room.path = room.id+'?'+naming.toUrlDecoration(room.name);
+		let	author = req.user.name,
 			messageLines = [];
 		room.path = room.id+'?'+naming.toUrlDecoration(room.name);
 		if (!exports.checkAtLeast(room.auth, 'admin')) {
 			return server.renderErr(res, "Admin auth is required");
 		}
-		if (r.dialog) {
+		if (room.dialog) {
 			return server.renderErr(res, "You can't change authorizations of a dialog room");
 		}
-		var m, modifiedUser, actions = [];
-		for (var key in req.body) {
+		let	m,
+			modifiedUser,
+			actions = [];
+		for (let key in req.body) {
 			if ((m = key.match(/^answer_request_(\d+)$/))) {
-				var	accepted = req.body[key]==='grant',
+				let	accepted = req.body[key]==='grant',
 					denyMessage = req.body['deny_message_'+m[1]];
-				modifiedUser = await this.getUserById(+m[1]);
+				modifiedUser = await con.getUserById(+m[1]);
 				if (accepted) {
 					messageLines.push(`@${author} gave *write* right to @${modifiedUser.name}.`);
 					actions.push({cmd:'insert_auth', auth:'write', user:modifiedUser.id});
@@ -126,27 +128,27 @@ exports.appPostAuths = function(req, res){
 				ws.emitAccessRequestAnswer(room.id, modifiedUser.id, accepted, denyMessage);
 			} else if ((m = key.match(/^insert_auth_(\d+)$/))) {
 				if (req.body[key]!='none') {
-					modifiedUser = await this.getUserById(+m[1]);
-					var auth = req.body[key];
+					modifiedUser = await con.getUserById(+m[1]);
+					let auth = req.body[key];
 					messageLines.push(`@${author} gave *${auth}* authorization to @${modifiedUser.name}.`);
 					actions.push({cmd:'insert_auth', auth, user:modifiedUser.id});
 				}
 			} else if ((m = key.match(/^change_auth_(\d+)$/))) {
-				var new_auth = req.body[key];
-				modifiedUser = await this.getUserById(+m[1]);
+				let new_auth = req.body[key];
+				modifiedUser = await con.getUserById(+m[1]);
 				if (new_auth==='none') {
 					actions.push({cmd:'delete_auth', user:modifiedUser.id});
 					messageLines.push(`@${author} removed all rights of @${modifiedUser.name}.`);
-					if (r.private) {
-						ws.throwOut(modifiedUser.id, r.id, 'Your rights to this room have been revoked');
-						ws.throwOut(modifiedUser.id, 'w'+r.id);
+					if (room.private) {
+						ws.throwOut(modifiedUser.id, room.id, 'Your rights to this room have been revoked');
+						ws.throwOut(modifiedUser.id, 'w'+room.id);
 					}
 				} else {
 					messageLines.push(`@${author} changed rights of @${modifiedUser.name} to *${new_auth}*.`);
 					actions.push({cmd:'update_auth', user:modifiedUser.id, auth:new_auth});
 				}
 			} else if ((m = key.match(/^unban_(\d+)_(\d+)$/))) {
-				var banid = +m[1], banned = +m[2];
+				let banid = +m[1], banned = +m[2];
 				actions.push({cmd:'unban', id:banid, user:banned});
 				actions.push({cmd:'delete_ar', user:banned});
 				ws.emitAccessRequestAnswer(room.id, banned, true);
@@ -155,35 +157,30 @@ exports.appPostAuths = function(req, res){
 		if (messageLines.length) {
 			ws.botMessage(null, room.id, messageLines.join("\n"));
 		}
-		return this.changeRights(actions, req.user.id, r);
-	}).then(function(){
+		await con.changeRights(actions, req.user.id, room);
 		res.redirect(server.roomUrl(room));
-	}).catch(db.NoRowError, function(){
-		server.renderErr(res, "room not found");
-	}).catch(function(err){
+	}, function(err){
 		server.renderErr(res, err);
-	}).finally(db.off);
+	});
 }
 
-exports.wsOnBan = function(shoe, o){
+exports.wsOnBan = async function(shoe, o){
 	if (!shoe.room) return console.log('No room in wsOnBan');
 	o.banner = shoe.publicUser.id;
 	o.bannername = shoe.publicUser.name;
-	return db.on([shoe.room.id, o.banned])
-	.spread(db.getAuthLevel)
-	.then(function(obj){
-		var bannedAuth = obj ? obj.auth : null;
+	await db.do(async function(con){
+		let row = await con.getAuthLevel(shoe.room.id, o.banned);
+		let bannedAuth = row ? row.auth : null;
 		if (bannedAuth==="own") throw "A room owner cannot be banned";
-		var bannerAuth = shoe.room.auth;
+		let bannerAuth = shoe.room.auth;
 		if (bannedAuth==="admin" && bannerAuth!=="own") throw "Only a room owner can ban an admin";
 		if (bannerAuth!=="admin" && bannerAuth!=="own") throw "Only an owner or an admin can ban a user";
-		var now = Date.now()/1000|0;
-		return this.insertBan(shoe.room.id, o.banned, now, now+o.duration, shoe.publicUser.id, o.reason);
-	}).then(function(){
+		let now = Date.now()/1000|0;
+		await con.insertBan(shoe.room.id, o.banned, now, now+o.duration, shoe.publicUser.id, o.reason);
 		shoe.emitToRoom('ban', o);
 		ws.throwOut(o.banned, shoe.room.id, 'You have been banned from this room for ' + o.nb + ' ' + o.unit);
 		ws.throwOut(o.banned, 'w'+shoe.room.id);
-	}).catch(function(err){
+	}, function(err){
 		shoe.error('error in ban : '+err.toString());
-	}).finally(db.off);
+	});
 }
