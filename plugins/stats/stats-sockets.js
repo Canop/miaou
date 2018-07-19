dd371420023485f3099d3162ada453deab0dd14b
Canop <cano.petrole@gmail.com>
2018-07-19 22:15:38 +0200

    stats on user agents in !!stats sockets

--- plugins/stats/stats-sockets.js
+++ plugins/stats/stats-sockets.js
@@ -1,27 +1,38 @@
 // !!stats sockets command
 
+function incr(map, key){
+	if (!key) return;
+	map.set(key, (map.get(key)||0)+1);
+}
+
 exports.doStats = function(ct, io){
-	var	transports = new Map(), // map transportName -> number
+	let	transports = new Map, // map transportName -> number
 		nbSockets = 0,
 		nbInvalidSockets = 0,
-		users = new Map(); // map userId -> number
-	for (var clientId in io.sockets.connected) {
-		var socket = io.sockets.connected[clientId];
+		users = new Map, // map userId -> number
+		userAgents = new Map; // map userAgent -> number
+	for (let clientId in io.sockets.connected) {
+		let socket = io.sockets.connected[clientId];
 		nbSockets++;
 		try {
-			var transportName = socket.conn.transport.name;
-			transports.set(transportName, (transports.get(transportName)||0) + 1);
-			var userId = socket.publicUser.id;
-			users.set(userId, (users.get(userId)||0) + 1);
+			incr(transports, socket.conn.transport.name);
+			incr(users, socket.publicUser.id);
+			if (socket.request && socket.request.headers) {
+				incr(userAgents, socket.request.headers["user-agent"]);
+			}
 		} catch (e) {
 			console.log('error in stats sockets', e);
 			nbInvalidSockets++;
 		}
-		var c = 'Sockets Statistics\n';
-		c += nbSockets + ' sockets for ' + users.size + ' different users\n';
+		let c = 'Sockets Statistics\n';
 		if (nbInvalidSockets) c += nbInvalidSockets + ' invalid sockets\n';
 		c += 'Transport|Connections\n-|:-:\n';
 		transports.forEach(function(nb, name){
+			c += name + '|' + nb + '\n';
+		});
+		c += nbSockets + ' sockets for ' + users.size + ' different users\n';
+		c += 'User Agent|Connections\n-|:-:\n';
+		userAgents.forEach(function(nb, name){
 			c += name + '|' + nb + '\n';
 		});
 		ct.reply(c, ct.nostore = c.length>800);
