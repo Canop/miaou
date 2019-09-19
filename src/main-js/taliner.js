e31ac99ea5c07d1b9fb8c450fdb89f978cfd348f
Canop <cano.petrole@gmail.com>
2019-09-19 13:40:55 +0200

    fix down arrow not goint to next message on Chrome
    Related to https://github.com/Canop/taliner.js/issues/1

--- src/main-js/taliner.js
+++ src/main-js/taliner.js
@@ -6,7 +6,7 @@ $.fn.taliner = function(){
 		$s1 = $('<span>').text('a').appendTo($d),
 		$s2 = $('<span>').appendTo($d),
 		lh =  $s1.height();
-    
+
 	$d.css({
 		width: $t.width(),
 		height: $t.height(),
@@ -27,15 +27,15 @@ $.fn.taliner = function(){
 		se = input.selectionEnd,
 		v = input.value,
 		res = {};
-	
+
 	lh = $s1.height();
 	$s1.text(v);
 	res.linesNumber = $s1.height()/lh|0;
-		
+
 	$s1.text(v.slice(0, se));
 	$s2.text(v.slice(se));
 	res.caretOnFirstLine = input.selectionEnd===0 || ($s1.height()<=lh && $s1.offset().top===$s2.offset().top);
-	res.caretOnLastLine = $s2.height()===lh;
+	res.caretOnLastLine = $s2.height()<=lh;
 
 	$d.remove();
 	return res;
