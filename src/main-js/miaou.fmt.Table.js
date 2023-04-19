7114e3c079bf0b06d76eceb195234326e4f79cd7
Canop <cano.petrole@gmail.com>
2023-04-19 16:02:06 +0200

    markdown tables: add colspan support, drop rowspan support

--- src/main-js/miaou.fmt.Table.js
+++ src/main-js/miaou.fmt.Table.js
@@ -1,12 +1,10 @@
 miaou(function(fmt){
 
 	var	regularcoldefregex = /^\s*[:\-]*(\|[:\-]+)*\|?\s*$/,
-		rowchange = {},
 		nextId = 1;
 
 	var Table = fmt.Table = function(cols){
 		this.coldefstr = cols;
-		this.regular = !/\+/.test(cols); // A "regular" table may accept multi-lines cells
 		var id = this.id = 'tbl'+nextId++;
 		this.multiline = false;
 		this.nbcols = 0;
@@ -20,23 +18,15 @@ miaou(function(fmt){
 		this.lines = [];
 	}
 	Table.prototype.push = function(row){
-		var cells = row.match(/[^|]+/g);
-		if (cells) {
-			this.lines.push(cells);
-			this.nbcols = Math.max(this.nbcols, cells.length);
-		} else {
-			this.lines.push([]);
-		}
+		var cells = row.replace(/(^\||\|$)/, "").split("|");
+		this.lines.push(cells);
+		this.nbcols = Math.max(this.nbcols, cells.length);
 	}
 	// add to the table if it looks like part of it, and return true
 	// doesn't add if it's not compatible, and return false
 	Table.prototype.read = function(s){
 		if (/^\s*$/.test(s)) return false;
 		if (regularcoldefregex.test(s)) {
-			if (this.regular) {
-				this.multiline = true;
-				this.lines.push(rowchange);
-			}
 			return true;
 		}
 		if (s===this.coldefstr) {
@@ -53,46 +43,31 @@ miaou(function(fmt){
 		if (!cc.length) return '';
 		var	h = '<tr>',
 			tag = ishead ? 'th' : 'td';
+		let cells = []; // [{content, colspan}]
 		for (var i=0; i<cc.length; i++) {
-			h += '<'+tag;
-			if (i===cc.length-1 && i<this.nbcols-1) h += ' colspan='+(this.nbcols-i);
-			h += '>'+cc[i]+'</'+tag+'>';
+			if (cc[i] || i==0) {
+				cells.push({content:cc[i], colspan:1});
+			} else {
+				cells[cells.length-1].colspan++;
+			}
 		}
+		cells[cells.length-1].colspan += this.nbcols - cc.length;
+		cells.forEach(c => {
+			h += '<'+tag;
+			if (c.colspan>1) h += ' colspan='+c.colspan;
+			h += '>'+c.content+'</'+tag+'>';
+		});
 		return h+'</tr>';
 	}
 	Table.prototype.html = function(username){
 		var	h = '<div class=tablewrap>';
 		h += this.style ? '<table id='+this.id+'><style scoped>'+this.style+'</style>' : '<table>';
-		if (this.multiline) {
-			var currentRow;
-			this.lines.forEach(function(line, il){
-				if (line===rowchange) {
-					if (currentRow) h += this.toRow(currentRow, false);
-					currentRow = null;
-				} else {
-					var row = line.map(function(c){ return fmt.mdStringToHtml(c.trim(), username) });
-					if (!il) {
-						h += this.toRow(row, true);
-					} else if (currentRow) {
-						for (var i=0; i<row.length; i++) {
-							if (i<currentRow.length) currentRow[i] += '<br>'+ row[i];
-							else currentRow[i] = row[i];
-						}
-
-					} else {
-						currentRow = row;
-					}
-				}
-			}, this);
-			if (currentRow) h += this.toRow(currentRow, false);
-		} else {
-			h += this.lines.map(function(r, ir){
-				return this.toRow(
-					r.map(function(c){ return fmt.mdStringToHtml(c.trim(), username) }),
-					!ir
-				);
-			}, this).join('');
-		}
+		h += this.lines.map(function(r, ir){
+			return this.toRow(
+				r.map(function(c){ return fmt.mdStringToHtml(c, username) }),
+				!ir
+			);
+		}, this).join('');
 		h += '</table></div>';
 		return h;
 	}
